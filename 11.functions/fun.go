90253e550d7ad6eede484a701c8e3658c678628c
Mardok95 <adimartino95@gmail.com>
2022-10-10 17:15:41 +0200

    adding extra exercise

---
+++ 11.functions/fun.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+// You can define your functions or you can go here to find the default functions:
+// https://pkg.go.dev/std
+// The delcaration of a function is:
+// func nameOfFunction(nameOfParameter typeOfParamenter) typeOfResult
+
+func kelvinToCelsius(k float64) float64 { // Function that need a float64 para
+
+}
+func main() {
+	fmt.Println("Example of functions in Go")
+
+}
