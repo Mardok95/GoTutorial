efed17424c2fc21609b21bf205302814b91c04aa
Mardok95 <adimartino95@gmail.com>
2022-10-26 17:21:19 +0200

    I'm making exercism exercise and i use this file to test the code

--- exercise.go
+++ exercise.go
@@ -2,8 +2,42 @@
 
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func main(){
-	fmt.Prinln("File used for random exercise")
+// SeedWithTime seeds math/rand with the current computer time.
+func SeedWithTime() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RollADie returns a random int d with 1 <= d <= 20.
+func RollADie() int {
+	n := rand.Intn(20-1) + 1
+	return n
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+	m := 0.0 + rand.Float64()*(12.0-0.0)
+	return m
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	animal := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(animal), func(i, j int) {
+		animal[i], animal[j] = animal[j], animal[i]
+	})
+	return animal
+}
+
+func main() {
+	SeedWithTime()
+	fmt.Println(RollADie())
+	fmt.Println(GenerateWandEnergy())
+	fmt.Println(ShuffleAnimals())
+
 }
